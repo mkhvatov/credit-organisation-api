0449f6cceb27038f6e3c4eba31873c789412b6ec
mkhvatov <matveykhvatov@gmail.com>
2018-09-24 16:01:37 +0300

    entrypoint.sh added

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+python manage.py loaddata --format json credit/fixtures/initial_data.json
+
+# Start server
+echo "Starting server"
+python manage.py runserver 0.0.0.0:8000
